6a3061e53e4cd1246bb13391f3facdf7753ef056
sats17 <satishkumbhar9999@gmail.com>
2021-02-08 13:46:42 +0530

    Added basic ec2 example

---
+++ aws-examples/ec2-instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region     = "ap-south-1"
+    access_key = "client-key"
+    secret_key = "client-secret"    
+}
+
+resource "aws_instance" "my-own-ec2-server" {
+    ami = "ami-0db0b3ab7df22e366"
+    instance_type = "t2.micro"
+    tags = {
+      "ec2-server" = "ubunutu"
+    }
+}
